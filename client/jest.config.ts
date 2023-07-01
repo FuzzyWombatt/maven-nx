02b36dc416dc306493bbd803ec78191680efcbae
fuzzywombatt <ryangibson365@gmail.com>
2023-06-30 19:46:41 -0500

    add sonar reporter format

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,15 +1,26 @@
 /* eslint-disable */
 export default {
-  displayName: 'client',
-  preset: './jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': "@swc/jest",
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: './coverage/client',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+    displayName: 'client',
+    preset: './jest.preset.js',
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+        '^.+\\.[tj]sx?$': '@swc/jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: './coverage/client',
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+        '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+    ],
+    coveragePathIgnorePatterns: ['./mocks'],
+    reporters: [
+        'default',
+        [
+            'jest-sonar',
+            {
+                outputDirectory: './coverage/client',
+                outputName: 'jest-sonar-report.xml',
+            },
+        ],
+    ],
 };
