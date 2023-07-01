840b72997f3691859c857c796b6394e7ff7c77ee
fuzzywombatt <ryangibson365@gmail.com>
2023-06-30 19:05:50 -0500

    initial folder structure and dev-deps for client setup

---
+++ client/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'client',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': "@swc/jest",
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/client',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
---
+++ client/webpack.config.js
@@ -0,0 +1,16 @@
+const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPLugin')
+
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), withReact(), (config) => {
+
+  return merge(config, {
+    output: {
+      uniqueName: 'client',
+      publicPath: 'auto',
+      scriptType: 'text/javascript'
+    }
+  })
+});
