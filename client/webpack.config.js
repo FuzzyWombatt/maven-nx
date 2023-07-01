b8c785dda61c1ebf6e628ada677ac6b33467ea30
fuzzywombatt <ryangibson365@gmail.com>
2023-06-30 19:36:40 -0500

    continue formatting and setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,18 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
-const { merge } = require('webpack-merge')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPLugin')
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPLugin');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-
-  return merge(config, {
-    output: {
-      uniqueName: 'client',
-      publicPath: 'auto',
-      scriptType: 'text/javascript'
-    }
-  })
+    return merge(config, {
+        optimization: {
+            runtimeChunk: false,
+        },
+        output: {
+            uniqueName: 'client',
+            publicPath: 'auto',
+            scriptType: 'text/javascript',
+        },
+    });
 });
